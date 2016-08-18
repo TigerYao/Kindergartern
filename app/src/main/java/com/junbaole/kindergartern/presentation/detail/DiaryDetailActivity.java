5113766a543b86720cf6853dbbe35cf026d8169f
yaohu <yaohu201203@gmail.com>
2016-08-18 20:13:44 +0800

    add

---
+++ app/src/main/java/com/junbaole/kindergartern/presentation/detail/DiaryDetailActivity.java
@@ -0,0 +1,13 @@
+package com.junbaole.kindergartern.presentation.detail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiaryDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary_detail);
+    }
+}
