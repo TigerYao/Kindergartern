ab2d86a69598c55510d74168df752db568606467
yaohu <yaohu201203@gmail.com>
2016-09-13 20:39:51 +0800

    add

---
+++ CoreSDK/src/com/chat/util/presentation/viewfeatures/AgreeNewFriend.java
@@ -0,0 +1,9 @@
+package com.chat.util.presentation.viewfeatures;
+
+/**
+ * Created by admin on 16/3/11.
+ */
+public interface AgreeNewFriend extends MvpView {
+
+    void jumpIntoProfileActicity();
+}
